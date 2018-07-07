070bae62505330d4acf305a5bce9ce79fe967755
Eric Busboom <eric@civicknowledge.com>
2018-07-07 10:42:57 -0700

    Adding files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='jupyterhub-redmine-authenticator',
+    version='0.1',
+    description='Redmine authenticator for JupyterHub',
+    url='https://github.com/ericbusboom/jupyterhub-redmine-authenticator.git',
+    author='Eric Busboom',
+    author_email='eric@busboom.org',
+    license='2 Clause BSD',
+    packages=['redmineauthenticator'],
+)
