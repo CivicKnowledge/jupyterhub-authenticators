b233a7cc0a6d30c03b6e01961d0edea6052cba6b
Eric Busboom <eric@civicknowledge.com>
2018-09-27 18:17:11 -0700

    hacking

--- jhauth/github.py
+++ jhauth/github.py
@@ -2,22 +2,25 @@
 Custom Github authenticator
 """
 from oauthenticator.github import GitHubOAuthenticator as _GitHubOAuthenticator
+from tornado import gen, web
 
 class GitHubOAuthenticator(_GitHubOAuthenticator):
 
+    @gen.coroutine
     def authenticate(self, handler, data=None):
         r = super().authenticate(handler, data)
-        self.log.info("!!!! authenticate {}".format(r))
+        self.log.info("!!!! authenticate {} {}".format(r))
         return r
 
+    @gen.coroutine
     def get_authenticated_user(self, handler, data):
         r = super().get_authenticated_user(handler, data)
-        self.log.info("!!!! get_authenticated_user {}".format(r))
+        self.log.info("!!!! get_authenticated_user {} {} ".format(r))
         return r
 
     def check_whitelist(self, username):
         r = super().check_whitelist(username)
-        self.log.info("!!!! check_whitelist {}".format(r))
+        self.log.info("!!!! check_whitelist {} {}".format(r))
         return r
 
 
