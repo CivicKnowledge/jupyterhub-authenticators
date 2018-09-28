298b2f2748fa50c71837feb4990ef16bbbc590f0
Eric Busboom <eric@civicknowledge.com>
2018-09-27 17:05:20 -0700

    .

--- jhauth/github.py
+++ jhauth/github.py
@@ -7,17 +7,17 @@ class GitHubOAuthenticator(_GitHubOAuthenticator):
 
     def authenticate(self, handler, data=None):
         r = super().authenticate(handler, data)
-
+        print("!!!! authenticate", r)
         return r
 
     def get_authenticated_user(self, handler, data):
         r = super().get_authenticated_user(handler, data)
-
+        print("!!!! get_authenticated_user", r)
         return r
 
     def check_whitelist(self, username):
         r = super().check_whitelist(username)
-
+        print("!!!! check_whitelist", r)
         return r
 
 
