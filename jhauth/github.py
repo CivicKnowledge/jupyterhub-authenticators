cf126184f0cb2771222138a0fec8022abe333cc6
Eric Busboom <eric@civicknowledge.com>
2018-10-17 15:57:24 -0700

    hooks

--- jhauth/github.py
+++ jhauth/github.py
@@ -10,7 +10,7 @@ from oauthenticator.github import _api_headers, GITHUB_API, GITHUB_PROTOCOL
 from os import getenv
 from tornado import gen
 from tornado.httpclient import HTTPRequest, AsyncHTTPClient
-
+from tornado.httpclient import HTTPRequest, AsyncHTTPClient, HTTPError
 
 class GitHubOAuthenticator(_GitHubOAuthenticator):
 
@@ -57,11 +57,88 @@ class GitHubOAuthenticator(_GitHubOAuthenticator):
 
         return False
 
+    @gen.coroutine
     def authenticate(self, handler, data=None):
+        """We set up auth_state based on additional GitHub info if we
+        receive it.
+        """
+        code = handler.get_argument("code")
+        # TODO: Configure the curl_httpclient for tornado
+        http_client = AsyncHTTPClient()
 
-        r = super().authenticate(handler, data)
-        self.log.warning('XXX '+str(r))
-        return r
+        # Exchange the OAuth code for a GitHub Access Token
+        #
+        # See: https://developer.github.com/v3/oauth/
+
+        # GitHub specifies a POST request yet requires URL parameters
+        params = dict(
+            client_id=self.client_id,
+            client_secret=self.client_secret,
+            code=code
+        )
+
+        url = url_concat("%s://%s/login/oauth/access_token" % (GITHUB_PROTOCOL, GITHUB_HOST),
+                         params)
+
+        req = HTTPRequest(url,
+                          method="POST",
+                          headers={"Accept": "application/json"},
+                          body=''  # Body is required for a POST...
+                          )
+
+        resp = yield http_client.fetch(req)
+        resp_json = json.loads(resp.body.decode('utf8', 'replace'))
+
+        if 'access_token' in resp_json:
+            access_token = resp_json['access_token']
+        elif 'error_description' in resp_json:
+            raise HTTPError(403,
+                "An access token was not returned: {}".format(
+                    resp_json['error_description']))
+        else:
+            raise HTTPError(500,
+                "Bad response: %s".format(resp))
+
+        # Determine who the logged in user is
+        req = HTTPRequest("%s://%s/user" % (GITHUB_PROTOCOL, GITHUB_API),
+                          method="GET",
+                          headers=_api_headers(access_token)
+                          )
+        resp = yield http_client.fetch(req)
+        resp_json = json.loads(resp.body.decode('utf8', 'replace'))
+
+        username = resp_json["login"]
+        # username is now the GitHub userid.
+        if not username:
+            return None
+        # Check if user is a member of any whitelisted organizations.
+        # This check is performed here, as it requires `access_token`.
+        if self.github_organization_whitelist:
+            for org in self.github_organization_whitelist:
+                user_in_org = yield self._check_organization_whitelist(org, username, access_token)
+                if user_in_org:
+                    break
+            else:  # User not found in member list for any organisation
+                self.log.warning("User %s is not in org whitelist", username)
+                return None
+        userdict = {"name": username}
+        # Now we set up auth_state
+        userdict["auth_state"] = auth_state = {}
+        # Save the access token and full GitHub reply (name, id, email) in auth state
+        # These can be used for user provisioning in the Lab/Notebook environment.
+        # e.g.
+        #  1) stash the access token
+        #  2) use the GitHub ID as the id
+        #  3) set up name/email for .gitconfig
+        auth_state['access_token'] = access_token
+        # store the whole user model in auth_state.github_user
+        auth_state['github_user'] = resp_json
+        # A public email will return in the initial query (assuming default scope).
+        # Private will not.
+
+        self.log.warning('XXX {}'.format(userdict))
+
+        return userdict
 
 
 async def pre_spawn_hook(spawner):
